910f69e7bc0223c18447ef79ca2af885b3fb1e15
Josh Gross <git@joshmgross.com>
2026-03-13 11:29:31 -0400

    Add a z-index to tooltips

--- components/post-summary.tsx
+++ components/post-summary.tsx
@@ -107,7 +107,7 @@ export function PostSummary({
                 side="bottom"
                 sideOffset={4}
                 className={classNames(
-                  'max-w-[260px] px-3 py-1.5 rounded shadow-lg bg-secondary-inverse text-secondary-inverse sm:max-w-sm',
+                  'max-w-[260px] px-3 py-1.5 rounded shadow-lg bg-secondary-inverse text-secondary-inverse sm:max-w-sm z-10',
                   likeCount === 0 && 'hidden'
                 )}
               >
